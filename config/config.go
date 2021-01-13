a0dabcc19d3e282cb7d4d76523e48f1540b34c21
Thanh Vu <thanhauco@gmail.com>
2021-01-13 01:51:00 -0800

    Load Config from Env

--- config/config.go
+++ config/config.go
@@ -1,5 +1,16 @@
 package config
+import (
+    "os"
+    "strconv"
+)
 type Config struct {
     Port int
     Target string
+}
+func Load() *Config {
+    p, _ := strconv.Atoi(os.Getenv("PORT"))
+    if p == 0 { p = 8080 }
+    t := os.Getenv("TARGET")
+    if t == "" { t = "http://localhost:9000" }
+    return &Config{Port: p, Target: t}
 }
