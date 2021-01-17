7e8f6a70c04015c053e2e097b0addaf0c8093064
Thanh Vu <thanhauco@gmail.com>
2021-01-16 18:40:00 -0800

    Wire Proxy in Main

--- main.go
+++ main.go
@@ -1,3 +1,14 @@
 package main
-import "fmt"
-func main() { fmt.Println("Order Gateway") }
+import (
+    "net/http"
+    "order-gateway/config"
+    "order-gateway/proxy"
+    "fmt"
+)
+func main() {
+    c := config.Load()
+    p := proxy.New(c.Target)
+    http.Handle("/", p)
+    fmt.Printf("Listening on %d\n", c.Port)
+    http.ListenAndServe(fmt.Sprintf(":%d", c.Port), nil)
+}
