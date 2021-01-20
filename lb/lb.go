3591c10458e21e370829e5513cec0da5e5a45f70
Thanh Vu <thanhauco@gmail.com>
2021-01-20 07:34:00 -0800

    Implement Round Robin

--- lb/lb.go
+++ lb/lb.go
@@ -1,6 +1,21 @@
 package lb
-import "net/url"
+import (
+    "net/url"
+    "sync/atomic"
+)
 type LoadBalancer struct {
     targets []*url.URL
     current uint64
+}
+func New(targets []string) *LoadBalancer {
+    var urls []*url.URL
+    for _, t := range targets {
+        u, _ := url.Parse(t)
+        urls = append(urls, u)
+    }
+    return &LoadBalancer{targets: urls}
+}
+func (l *LoadBalancer) Next() *url.URL {
+    i := atomic.AddUint64(&l.current, 1)
+    return l.targets[i % uint64(len(l.targets))]
 }
