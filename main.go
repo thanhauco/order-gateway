0551ae55ed855ebffdb4b478632d21cb43f7a8f1
Thanh Vu <thanhauco@gmail.com>
2021-01-28 00:10:00 -0800

    Update Main for LB

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ import (
     "net/http"
     "order-gateway/config"
     "order-gateway/proxy"
+    "order-gateway/lb"
     "fmt"
 )
 func main() {
     c := config.Load()
-    p := proxy.New(c.Target)
+    l := lb.New([]string{c.Target, "http://backup:9000"})
+    p := proxy.New(l)
     http.Handle("/", p)
     fmt.Printf("Listening on %d\n", c.Port)
     http.ListenAndServe(fmt.Sprintf(":%d", c.Port), nil)
