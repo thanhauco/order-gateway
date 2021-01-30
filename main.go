05abaab0e92860b7943f0db794d357ce482bc4fa
Thanh Vu <thanhauco@gmail.com>
2021-01-30 03:12:00 -0800

    Chain Middleware

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
     "order-gateway/config"
     "order-gateway/proxy"
     "order-gateway/lb"
+    "order-gateway/middleware"
     "fmt"
 )
 func main() {
     c := config.Load()
-    l := lb.New([]string{c.Target, "http://backup:9000"})
+    l := lb.New([]string{c.Target})
     p := proxy.New(l)
-    http.Handle("/", p)
+    h := middleware.Logger(middleware.Auth(p))
+    http.Handle("/", h)
     fmt.Printf("Listening on %d\n", c.Port)
     http.ListenAndServe(fmt.Sprintf(":%d", c.Port), nil)
 }
