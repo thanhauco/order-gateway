819c9b17583ed0b0574ceac02f1d30ff4883c0fc
Thanh Vu <thanhauco@gmail.com>
2021-02-09 06:59:00 -0800

    Implement Allow method

--- limiter/limiter.go
+++ limiter/limiter.go
@@ -5,3 +5,9 @@ type Limiter struct {
     mu sync.Mutex
 }
 func New() *Limiter { return &Limiter{tokens: make(map[string]int)} }
+func (l *Limiter) Allow(ip string) bool {
+    l.mu.Lock()
+    defer l.mu.Unlock()
+    l.tokens[ip]++
+    return l.tokens[ip] < 100
+}
