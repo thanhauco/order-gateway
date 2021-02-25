99a60779c1f47058697a74538d5c3b83d9b5e83f
Thanh Vu <thanhauco@gmail.com>
2021-02-25 14:53:00 -0800

    Add Shutdown logic

--- main.go
+++ main.go
@@ -7,14 +7,27 @@ import (
     "order-gateway/middleware"
     "order-gateway/handlers"
     "fmt"
+    "os"
+    "os/signal"
+    "context"
+    "time"
 )
 func main() {
     c := config.Load()
     l := lb.New([]string{c.Target})
     p := proxy.New(l)
     h := middleware.RateLimit(middleware.Measure(middleware.Logger(middleware.Auth(p))))
-    http.Handle("/", h)
-    http.HandleFunc("/metrics", handlers.Stats)
-    fmt.Printf("Listening on %d\n", c.Port)
-    http.ListenAndServe(fmt.Sprintf(":%d", c.Port), nil)
+    mux := http.NewServeMux()
+    mux.Handle("/", h)
+    mux.HandleFunc("/metrics", handlers.Stats)
+    srv := &http.Server{Addr: fmt.Sprintf(":%d", c.Port), Handler: mux}
+    go func() {
+        if err := srv.ListenAndServe(); err != nil { fmt.Println(err) }
+    }()
+    stop := make(chan os.Signal, 1)
+    signal.Notify(stop, os.Interrupt)
+    <-stop
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+    srv.Shutdown(ctx)
 }
