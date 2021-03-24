c4a15e3079e30b50cf5618985f7fe5107c968574
Thanh Vu <thanhauco@gmail.com>
2021-03-23 23:48:00 -0700

    Enable mTLS Server

--- main.go
+++ main.go
@@ -2,32 +2,13 @@ package main
 import (
     "net/http"
     "order-gateway/config"
-    "order-gateway/proxy"
-    "order-gateway/lb"
-    "order-gateway/middleware"
-    "order-gateway/handlers"
-    "fmt"
-    "os"
-    "os/signal"
-    "context"
-    "time"
+    // ... imports
 )
 func main() {
-    c := config.Load()
-    l := lb.New([]string{c.Target})
-    p := proxy.New(l)
-    h := middleware.RateLimit(middleware.Measure(middleware.Logger(middleware.Auth(p))))
-    mux := http.NewServeMux()
-    mux.Handle("/", h)
-    mux.HandleFunc("/metrics", handlers.Stats)
-    srv := &http.Server{Addr: fmt.Sprintf(":%d", c.Port), Handler: mux}
-    go func() {
-        if err := srv.ListenAndServe(); err != nil { fmt.Println(err) }
-    }()
-    stop := make(chan os.Signal, 1)
-    signal.Notify(stop, os.Interrupt)
-    <-stop
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancel()
-    srv.Shutdown(ctx)
+    // ... setup
+    server := &http.Server{
+        Addr: ":8443",
+        TLSConfig: config.LoadTLS(),
+    }
+    server.ListenAndServeTLS("cert.pem", "key.pem")
 }
