fa00741b8e08e7b51743f02928c53c180380c58e
Thanh Vu <thanhauco@gmail.com>
2021-06-01 19:35:00 -0700

    Add Pprof

--- main.go
+++ main.go
@@ -1,14 +1,3 @@
 package main
-import (
-    "net/http"
-    "order-gateway/config"
-    // ... imports
-)
-func main() {
-    // ... setup
-    server := &http.Server{
-        Addr: ":8443",
-        TLSConfig: config.LoadTLS(),
-    }
-    server.ListenAndServeTLS("cert.pem", "key.pem")
-}
+import _ "net/http/pprof"
+// ... existing
