b9a5f3041b11d01a20cb40f602c90c0ae511b968
Thanh Vu <thanhauco@gmail.com>
2021-06-08 01:53:00 -0700

    Add Config Validation

--- config/config.go
+++ config/config.go
@@ -1,16 +1,6 @@
 package config
-import (
-    "os"
-    "strconv"
-)
-type Config struct {
-    Port int
-    Target string
-}
-func Load() *Config {
-    p, _ := strconv.Atoi(os.Getenv("PORT"))
-    if p == 0 { p = 8080 }
-    t := os.Getenv("TARGET")
-    if t == "" { t = "http://localhost:9000" }
-    return &Config{Port: p, Target: t}
+import "errors"
+func (c *Config) Validate() error {
+    if c.Port == 0 { return errors.New("invalid port") }
+    return nil
 }
