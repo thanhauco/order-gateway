a690a3c4ad06ff29ae88460f78cc3c7d1389f4f5
Thanh Vu <thanhauco@gmail.com>
2021-06-13 04:17:00 -0700

    Add CORS

---
+++ middleware/cors.go
@@ -0,0 +1,7 @@
+package middleware
+func CORS(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        next.ServeHTTP(w, r)
+    })
+}
