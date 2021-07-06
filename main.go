4f1cdbec06638169ca50666d47c89c8e63682e0e
Thanh Vu <thanhauco@gmail.com>
2021-07-06 05:24:00 -0700

    Add Version Flag

--- main.go
+++ main.go
@@ -1,2 +1,9 @@
 package main
-// ... fix comments
+import "flag"
+var version = "1.0.0"
+func main() {
+    v := flag.Bool("v", false, "version")
+    flag.Parse()
+    if *v { println(version); return }
+    // ... rest
+}
